4cafe3d6f9bea094b4adb10eb156c3316e55099e
Jay Grey <jaygrey@yandex.ru>
2018-03-29 10:46:11 +0300

    add nginx conf

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
+# remove old settings
+rm /etc/nginx/sites-enabled/test.conf
+rm /etc/nginx/sites-enabled/default
+rm /home/box/web/uploads/.empty 2> /dev/null
+rm /home/box/web/public/css/.empty 2> /dev/null
+rm /home/box/web/public/img/.empty 2> /dev/null
+rm /home/box/web/public/js/.empty  2> /dev/null
+
 # copy nginx configuration
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
