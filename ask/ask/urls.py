27ab475a8ab5a67fbf3198707d5f3c581c5f50d3
Jay Grey <jaygrey@yandex.ru>
2018-03-30 10:40:55 +0300

    fix router

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from ask.qa.views import test
+from qa.views import test
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
