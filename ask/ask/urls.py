c09a81fbe59774c40356869047faa6c9af89c770
Jay Grey <jaygrey@yandex.ru>
2018-03-30 10:16:54 +0300

    add urls and default controller

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ask.qa.views import test
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', test, name='home'),
+    url(r'^login/$', test, name='login'),
+    url(r'^signup/$', test, name='signup'),
+    url(r'^question/(?P<id>\d+)/$', test, name='question'),
+    url(r'^ask/$', test, name='ask'),
+    url(r'^popular/$', test, name='popular'),
+    url(r'^new/$', test, name='new'),
 ]
