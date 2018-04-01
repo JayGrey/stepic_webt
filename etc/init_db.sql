63b6f096938bd7d7f0a85cf1b9e491ae573914bf
Jay Grey <jaygrey@yandex.ru>
2018-04-01 18:52:01 +0300

    fix init sql script

--- etc/init_db.sql
+++ etc/init_db.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS ask_db;
-DROP USER IF EXISTS 'django';
+DROP USER 'django';
 CREATE DATABASE  if not exists ask_db;
 CREATE USER 'django' IDENTIFIED BY 'secret';
 GRANT ALL ON ask_db.* TO 'django';
